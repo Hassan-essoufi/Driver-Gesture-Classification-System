8e5204cf02cd9f39bedd214c2c7046ac5f394684
hassan <hassan>
2025-12-18 21:58:55 +0100

    preprocessing

---
+++ src/preprocess.py
@@ -0,0 +1,53 @@
+import os
+import cv2
+import pandas as pd
+import numpy as np
+from typing import Tuple
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+#import albumentations as A
+#from albumentations.pytorch import ToTensorV2
+
+
+#Image loading
+def load_image(image_path):
+    if not os.path.isfile(image_path):
+        raise FileNotFoundError(f"Image not found: {image_path}")
+
+    image = cv2.imread(image_path)
+
+    if image is None:
+        raise ValueError(f"Failed to load image: {image_path}")
+
+    # Convert BGR (OpenCV default) to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    return image
+img = load_image('C:/Users/hp/Desktop/Projects/Driver_Gesture_Detection_System/img_186.jpg')
+print(img)
+
+
+def resize_image(image, size=(224, 224)):
+    """
+    Resize image to target size.
+    """
+    if image is None:
+        raise ValueError("Input image is None")
+
+    if not isinstance(image, np.ndarray):
+        raise TypeError("Input image must be a numpy array")
+
+    # width, height
+    target_h, target_w = size[0] ,size[1]
+
+    resized_image = cv2.resize(
+        image,
+        (target_w, target_h),
+        interpolation=cv2.INTER_LINEAR
+    )
+
+    return resized_image
+
+print(resize_image(img).shape)
